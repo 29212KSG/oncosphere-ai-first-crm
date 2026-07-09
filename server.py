71f6ccca9938ec82141c3b882cf0b982b94838a1
Mukul Sharma <sharma29212k@gmail.com>
2026-07-09 22:48:19 +0530

    update timedate format

--- server.py
+++ server.py
@@ -448,7 +448,7 @@ async def process_nlp(req: ProcessRequest):
     
     def add_trace(node: str, node_type: str, message: str, data: Optional[Any] = None):
         trace_log.append({
-            "timestamp": datetime.utcnow().isoformat() + "Z",
+            "timestamp": datetime.istnow().isoformat() + "Z",
             "node": node,
             "type": node_type,
             "message": message,
